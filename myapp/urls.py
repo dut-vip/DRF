9ac5f226ed3d73e3345613dfbd627e8b351e34c7
Hoàng Minh Đức <zzz21022024a@gmail.com>
2024-09-29 12:33:56 +0700

    done lab4

--- myapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-from .views import story_list_view
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('stories/', story_list_view, name='story_list'),
-]
--- mysite/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from myapp.views import my_async_view, safe_view, non_cache_view, async_view_using_sync_function
-
-urlpatterns = [
-    path('', my_async_view, name='my_async_view'),
-    path('safe/', safe_view, name='safe_view'),
-    path('non-cache/', non_cache_view, name='non_cache_view'),
-    path('async-sync/', async_view_using_sync_function, name='async_sync_view'),
-]
